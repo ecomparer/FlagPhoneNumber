fa52e5cf9064079303662c61184d678420ac9e8e
ecomparer <31760799+ecomparer@users.noreply.github.com>
2020-06-13 17:12:12 +0300

    flag size

--- Sources/FPNTextField.swift
+++ Sources/FPNTextField.swift
@@ -11,7 +11,7 @@ import UIKit
 open class FPNTextField: UITextField {
 
 	/// The size of the flag button
-	@objc open var flagButtonSize: CGSize = CGSize(width: 32, height: 32) {
+	@objc open var flagButtonSize: CGSize = CGSize(width: 55, height: 40) {
 		didSet {
 			layoutIfNeeded()
 		}
